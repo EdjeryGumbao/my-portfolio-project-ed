85cb46a99312dc140d20be2a82bf2940f3fa077f
EJ <edisgumbao@gmail.com>
2024-03-11 15:31:02 +0800

    Add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Box, CssBaseline, Toolbar } from "@mui/material";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <Box sx={{ backgroundColor: "white" }}>
+      <CssBaseline />
       <Navbar />
-    </>
+      <Toolbar />
+    </Box>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,94 @@
-import React from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+
+import { useState } from "react";
+
+const drawerWidth = 240;
+const navItems = ["Home", "Projects", "Skills", "About Me", "Contact"];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const handleDrawerToggle = () => {
+    setDrawerOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "left" }}>
+              <ListItemText primary={item} />
+              {/* TODO: Add anchor links */}
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }} color="">
+      <AppBar component="nav" color="default">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            PORTFOLIO
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: "black" }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          variant="temporary"
+          open={drawerOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
 };
 
 export default Navbar;
