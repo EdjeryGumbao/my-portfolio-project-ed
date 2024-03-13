1b96f570c7d9ac6b5ef8fd3ae853c229b5322485
EJ <edisgumbao@gmail.com>
2024-03-13 13:26:33 +0800

    Refactor header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,46 +3,44 @@ import headerPerson from "../assets/header-person.png";
 
 const Header = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        backgroundColor: "lightgray",
-      }}
-    >
-      <Grid container alignItems="center" pl={10}>
-        <Grid item xs={10} lg={5}>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
+    <Box>
+      <Box sx={{ bgcolor: "info.main" }}>
+        <Grid container alignItems="center" pl={10}>
+          <Grid item xs={12} lg={5}>
             <Box
-              component="img"
-              alt="header-person"
-              src={headerPerson}
-              sx={{ width: "80%", height: "auto", objectFit: "cover" }}
-            />
-          </Box>
-        </Grid>
-        <Grid item xs={10} lg={7}>
-          <Box sx={{ textAlign: { xs: "center", md: "left" }, my: { xs: 5 } }}>
-            <Typography variant="h6" sx={{ fontSize: { xs: 30, md: 20 } }}>
-              Edjery Gumbao
-            </Typography>
-            <Divider
               sx={{
-                mb: { xs: 3, lg: 0 },
-                display: { xs: "block", md: "none" },
+                display: "flex",
+                justifyContent: "center",
               }}
-            />
-            <Typography variant="h3" sx={{ fontWeight: "bold" }}>
-              Passionately Crafting Software Solutions
-            </Typography>
-          </Box>
+            >
+              <Box
+                component="img"
+                alt="header-person"
+                src={headerPerson}
+                sx={{ width: "80%", height: "auto", objectFit: "cover" }}
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={12} lg={7}>
+            <Box
+              sx={{ textAlign: { xs: "center", md: "left" }, my: { xs: 5 } }}
+            >
+              <Typography variant="h6" sx={{ fontSize: { xs: 30, md: 20 } }}>
+                Edjery Gumbao
+              </Typography>
+              <Divider
+                sx={{
+                  mb: { xs: 3, lg: 0 },
+                  display: { xs: "block", md: "none" },
+                }}
+              />
+              <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+                Passionately Crafting Software Solutions
+              </Typography>
+            </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 };
