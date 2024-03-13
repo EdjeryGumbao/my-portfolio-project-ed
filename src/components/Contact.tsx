4cff243140dd4c042dac62bc0c67f079abae1a59
EJ <edisgumbao@gmail.com>
2024-03-13 08:59:09 +0800

    Improve contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -13,39 +13,46 @@ import {
 const Contact = () => {
   return (
     <Box maxWidth={1200} mx="auto" my={15} px={5}>
-      <Typography
-        variant="h4"
-        sx={{ textAlign: "left", fontWeight: "bold", pb: 5 }}
-      >
-        Contact
-      </Typography>
-      <form action="https://formbold.com/s/9gp78" method="POST">
-        <Stack spacing={2}>
-          <FormControl>
-            <InputLabel htmlFor="email">Email</InputLabel>
-            <OutlinedInput id="email" name="email" type="email" label="Email" />
-          </FormControl>
-          <FormControl>
-            <InputLabel htmlFor="subject">Subject</InputLabel>
-            <OutlinedInput
-              id="subject"
-              name="subject"
-              type="text"
-              label="Subject"
+      <Stack spacing={5}>
+        <Typography variant="h4" sx={{ textAlign: "left", fontWeight: "bold" }}>
+          Contact
+        </Typography>
+        <Typography variant="h6">
+          Got questions or inquiries? Don't hesitate to reach out!
+        </Typography>
+        <form action="https://formbold.com/s/9gp78" method="POST">
+          <Stack spacing={2}>
+            <FormControl>
+              <InputLabel htmlFor="email">Email</InputLabel>
+              <OutlinedInput
+                id="email"
+                name="email"
+                type="email"
+                label="Email"
+              />
+            </FormControl>
+            <FormControl>
+              <InputLabel htmlFor="subject">Subject</InputLabel>
+              <OutlinedInput
+                id="subject"
+                name="subject"
+                type="text"
+                label="Subject"
+              />
+            </FormControl>
+            <TextField
+              id="message"
+              name="message"
+              label="Message"
+              multiline
+              rows={5}
             />
-          </FormControl>
-          <TextField
-            id="message"
-            name="message"
-            label="Message"
-            multiline
-            rows={5}
-          />
-          <Button type="submit" variant="contained" endIcon={<SendIcon />}>
-            Submit
-          </Button>
-        </Stack>
-      </form>
+            <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+              Submit
+            </Button>
+          </Stack>
+        </form>
+      </Stack>
     </Box>
   );
 };
