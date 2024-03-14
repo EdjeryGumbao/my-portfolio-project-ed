82a716561f00985583c0a629ae51150bc5981524
EJ <edisgumbao@gmail.com>
2024-03-14 12:50:38 +0800

    update websiteList

--- src/data/websiteList.ts
+++ src/data/websiteList.ts
@@ -1,8 +1,5 @@
-import edmoshified from "../assets/Images/Website/Edmoshified.png";
 import edgamehub from "../assets/Images/Website/EdGameHubH.png";
-import sampleImg from "../assets/sampleWebsite.png";
-
-// TODO Add the websites
+import edmoshified from "../assets/Images/Website/Edmoshified.png";
 
 const websitelist = [
   {
