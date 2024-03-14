7f9710f56f783cb8c3e9859f9f3aa489e62ec981
EJ <edisgumbao@gmail.com>
2024-03-14 12:42:29 +0800

    Add my projects

--- src/data/websiteList.ts
+++ src/data/websiteList.ts
@@ -1,27 +1,19 @@
+import edmoshified from "../assets/Images/Website/Edmoshified.png";
+import edgamehub from "../assets/Images/Website/EdGameHubH.png";
 import sampleImg from "../assets/sampleWebsite.png";
 
 // TODO Add the websites
 
 const websitelist = [
   {
-    label: "Youtube",
-    link: "https://www.youtube.com/",
-    img: sampleImg,
+    label: "Cloud Hosting Landing Page",
+    link: "https://edmoshified.vercel.app/",
+    img: edmoshified,
   },
   {
-    label: "Google",
-    link: "https://www.google.com/",
-    img: sampleImg,
-  },
-  {
-    label: "Gmail",
-    link: "https://www.gmail.com/",
-    img: sampleImg,
-  },
-  {
-    label: "Github",
-    link: "https://www.github.com/",
-    img: sampleImg,
+    label: "Game Hub",
+    link: "https://edgamehub.vercel.app/",
+    img: edgamehub,
   },
 ];
 
