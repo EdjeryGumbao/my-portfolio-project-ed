6d714d53f82964d460527e578721324cc59c52ca
edjerygumbao <egcgumbao@usep.edu.ph>
2024-03-19 19:43:58 +0800

    Updated Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { Box, Divider, Grid, Typography } from "@mui/material";
-import headerPerson from "../assets/Images/header-person.png";
+import headerPerson from "../assets/Images/header-image.png";
 
 const Header = () => {
   return (
     <Box>
       <Box sx={{ bgcolor: "info.main" }}>
         <Grid container alignItems="center" pl={10}>
-          <Grid item xs={10} lg={5}>
+          <Grid item xs={9} sm={5} md={6} lg={5}>
             <Box
               sx={{
                 display: "flex",
@@ -15,26 +15,26 @@ const Header = () => {
             >
               <Box
                 component="img"
-                alt="header-person"
+                alt="header-image"
                 src={headerPerson}
-                sx={{ width: "80%", height: "auto", objectFit: "cover" }}
+                sx={{ width: "100%", height: "auto", objectFit: "cover" }}
               />
             </Box>
           </Grid>
-          <Grid item xs={10} lg={7}>
+          <Grid item xs={9} sm={7} md={6} lg={7}>
             <Box
-              sx={{ textAlign: { xs: "center", md: "left" }, my: { xs: 5 } }}
+              sx={{ textAlign: { xs: "center", sm: "left" }, my: { xs: 5 } }}
             >
-              <Typography variant="h6" sx={{ fontSize: { xs: 30, md: 20 } }}>
+              <Typography variant="h6" sx={{ fontSize: { xs: 30, sm: 25, lg: 30 } }}>
                 Edjery Gumbao
               </Typography>
               <Divider
-                sx={{
+                sx={{ 
                   mb: { xs: 3, lg: 0 },
                   display: { xs: "block", md: "none" },
                 }}
               />
-              <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+              <Typography variant="h3" sx={{ fontWeight: "bold", fontSize: { xs: 30, lg: 50 } } }>
                 Passionately Crafting Software Solutions
               </Typography>
             </Box>
